a1c3516c282aa83a1f6f692ee54398886c63d712
Aymane Benhima <aymanebenhima@gmail.com>
2020-02-26 17:54:29 +0100

    implements attack, special attacks and heal

--- app.js
+++ app.js
@@ -10,6 +10,51 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function () {
+            this.monsterHealth   -= this.calculateDamage(3,10);
+            if (this.checkWin())
+                return;
+            this.monsterAttacks();
+        },
+        specialAttack: function() {
+            this.monsterHealth   -= this.calculateDamage(10,20);
+            if (this.checkWin())
+                return;
+            this.monsterAttacks();    
+        },
+        heal: function () {
+            if (this.playerHealth <= 90)
+                this.playerHealth += 10;
+            else
+                this.playerHealth = 100;
+            this.monsterAttacks();
+        },
+        giveUp: function() {
+
+        },
+        monsterAttacks: function() {
+            this.playerHealth    -= this.calculateDamage(5,12);
+            this.checkWin();
+        },
+        calculateDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You Win! Do want realy play again ?'))
+                    this.startGame();
+                else
+                    this.gameIsRunning = false;
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You Lost! Do want realy play again ?'))
+                    this.startGame();
+                else
+                    this.gameIsRunning = false;
+                return true;
+            }
+            return false;
         }
     }
 });
